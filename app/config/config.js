607e5c7093e8d6f4e739dd1732a91f3dee1ab8de
Justin2997 <justin.brlotte797@gmail.com>
2019-08-07 16:09:50 -0400

    Change BD

--- app/config/config.js
+++ app/config/config.js
@@ -1,19 +1,11 @@
 var config = {};
 
 config.database = {
-  host     : 'host.docker.internal',
-  user     : 'root',
-  password : 'root',
+  host     : '35.226.234.235',
+  user     : 'integration65',
+  password : 'integ65!',
   database : 'integ65',
-  port     : 8889
+  port     : 3306
 }
-
-/*config.database = {
-  host     : 'mysql.stackcp.com',
-  user     : 'integ65-3130314d92',
-  password : 'integration123',
-  database : 'integ65-3130314d92',
-  port     : 49741
-}*/
 
 module.exports = config;
